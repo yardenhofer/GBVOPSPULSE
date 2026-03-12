c5fd7da9669dbd08ae6606417f2700b13a8931c7
Yarden Hofer <yardenhofer@gmail.com>
2026-03-12 19:46:49 +0000

    File changes

--- functions/listUsers.ts
+++ functions/listUsers.ts
@@ -12,6 +12,16 @@ Deno.serve(async (req) => {
     return Response.json({ error: 'Forbidden: Admin access required' }, { status: 403 });
   }
 
+  const body = await req.json().catch(() => ({}));
+  const { action, user_id, data } = body;
+
+  // Update a user
+  if (action === 'update' && user_id && data) {
+    const updated = await base44.asServiceRole.entities.User.update(user_id, data);
+    return Response.json({ user: updated });
+  }
+
+  // Default: list users
   const users = await base44.asServiceRole.entities.User.list("-created_date", 200);
   return Response.json({ users });
 });
