df06e37f45fed01a2da9f165fccd31df11ee3aa7
Yarden Hofer <yardenhofer@gmail.com>
2026-03-16 19:44:07 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    safelist: [
+      "border-blue-500", "text-blue-500", "bg-blue-500",
+      "border-yellow-500", "text-yellow-500", "bg-yellow-500",
+      "border-red-500", "text-red-500", "bg-red-500",
+      "border-green-500", "text-green-500", "bg-green-500",
+    ],
   theme: {
   	extend: {
   		borderRadius: {
